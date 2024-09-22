6bb5c2d099234e88a55c16baf014c07d1798ad57
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-09-22 03:23:17 +0100

    init

---
+++ flowable-samples-basic/src/test/java/io/flowable/basic/AppTest.java
@@ -0,0 +1,17 @@
+package io.flowable.basic;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest {
+
+    @Test
+    void test() {
+        log.info("===================================");
+    }
+}
