cf1bedfe064c81608e975cc91754f0f450a8dc92
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-09-22 04:56:15 +0100

    init

---
+++ flowable-samples-basic/src/main/java/io/flowable/basic/HolidayRequest.java
@@ -0,0 +1,52 @@
+package io.flowable.basic;
+
+import java.util.Scanner;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class HolidayRequest {
+    private static Logger log = LoggerFactory.getLogger(HolidayRequest.class);
+    public static void main( String[] args ) {
+        log.info("==================###################");
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .deploy();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        log.info("Found process definition: {}", processDefinition.getName());
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Who are you?");
+        String employee = scanner.nextLine();
+
+        System.out.println("How many holidays do you want to request?");
+        Integer nrOfHolidays = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Why do you need them?");
+        String description = scanner.nextLine();
+    }
+}
